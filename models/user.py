dfc6223afe5053e3fb60f84b61062b3866526342
KMOkuda <kyo.trainee.sys.202205@gmail.com>
2025-11-03 18:07:31 +0900

    refs #89 ユーザーテーブルに作成日・更新日を追加

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, String
+from sqlalchemy import Column, BigInteger, String, DateTime, func
 
 from settings import Base
 
@@ -9,11 +9,16 @@ class User(Base):
     name = Column(String, nullable=False, unique=True)
     password_hash = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
+
 
     def __repr__(self):
-        return "<User('id={}, name={}', password_hash={}, email={})>".format(
+        return "<User('id={}, name={}', password_hash={}, email={}, created_at={}, updated_at={})>".format(
             self.id,
             self.name,
             self.password_hash,
-            self.email
+            self.email,
+            self.created_at,
+            self.updated_at
         )   
